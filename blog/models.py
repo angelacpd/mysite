0b21ebe265b58be1a659cd1cb97e800099ffb417
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-01-22 12:40:39 -0300

    Setting up the site admin.

--- blog/models.py
+++ blog/models.py
@@ -20,5 +20,5 @@ class Post(models.Model):
         def __unicode__(self):
             return u'%s' % self.title
 
-        def get_absolute_url(self):
-            return reverse('blog.views.post', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('blog.views.post', args=[self.slug])
