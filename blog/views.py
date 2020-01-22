d10be58cb505202acbf6b3d866b60f5288869227
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-01-22 13:19:29 -0300

    Dynamic template data.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 def index(request):
-    #return HttpResponse("Hey there! :)")
-    return render(request, 'index.html', {})
+    # return HttpResponse("Hey there! :)")
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
 
 
 def post(request):
